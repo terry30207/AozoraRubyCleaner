d317ab21e089d677fcdc6ad9eba01ab45cea15d9
terry30207 <32173096+terry30207@users.noreply.github.com>
2020-04-15 15:51:40 +0800

    Add files via upload
    改進難字轉譯邏輯

--- AozoraRubyCleaner.py
+++ AozoraRubyCleaner.py
@@ -14,7 +14,7 @@ import os
 
 url=input("請輸入網址\n")  # get page url 
 browser=webdriver.PhantomJS(executable_path=os.path.abspath(os.path.dirname(__file__))+'\\phantomjs.exe')       # "Selenium support for PhantomJS has been deprecated, please use headless versions of Chrome or Firefox instead" 
-                                                                                                                #NOT NOW.
+                                                                                                                # "NOT NOW"
 browser.get(url)
 pageSource = browser.page_source  # get html of the url
 soup = BeautifulSoup(pageSource, 'html.parser')# create BS object
@@ -39,26 +39,27 @@ soup_string = str(soup)
 aa=r'\<.*?\>'
 ext = re.sub(aa, '', soup_string)   #remove other tags
 
-#For those chars not in JIS, they notate like this: ※［＃「匈／（胃－田）」、U+80F7、32-本文-3］
+#For those chars not in JIS, they notate like this: ※［＃「匈／（胃－田）」、U+80F7、32-本文-3］or even like this:※形［＃「匈／（胃－田）」、U+80F7、32-本文-3］
 #Thus, codes below turns above notation back to correct char
+d=r'※.*?［＃.*?］'
+e=r'U\+....'
+g=r'※.*?［'
+res=re.search(d,ext)
+while res!= None:
+    res2=re.search(e,res.group(0))
+    res3=re.search(g,res.group(0))
+    chr=res2.group(0)[2]+res2.group(0)[3]+res2.group(0)[4]+res2.group(0)[5]
+    chr='\\u'+chr
+    chr=chr.encode('latin-1').decode('unicode_escape')
+    chr2=res3.group(0).strip("※").strip("［")   
+    ext=re.sub(d,chr2+chr,ext,1)
+    res=re.search(d,ext)
 
-a=r'※［.*?、'
-ext = re.sub(a, '', ext)#remove all the chars between '※' and the first '、'
 
-b=r'、.*?］'
-ext=re.sub(b,'',ext)#remove all the chars between second '、' and '］'
 
-c=r'U\+....'    
-res=re.search(c,ext)    #get the first unicode match the pattern 'U+XXXX'
-while res!= None:   #while found:
-    chr=res.group(0)[2]+res.group(0)[3]+res.group(0)[4]+res.group(0)[5]     #remove 'U+', str.strip just won't work and only god knows why
-    chr='\\u'+chr   #make pattern '\uXXXX'
-    chr=chr.encode('latin-1').decode('unicode_escape')      #convert to correct char
-    ext=re.sub(c,chr,ext,1)     #remove the pattern 'U+XXXX' and put the correct char back
-    res=re.search(c,ext)        #try to find next unicode
 print(ext)    #show in command line
 txtfile=open(os.path.abspath(os.path.dirname(__file__))+"\\"+ "ruby_free.txt" ,'w',encoding = 'utf8')
 txtfile.write(ext)
 txtfile.close()    #write to the txt file
-print("完成")
+print("完成!!")
 browser.close() #close phantomJS
